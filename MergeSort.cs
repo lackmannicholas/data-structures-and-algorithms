66b51d2f02f4a6285c2b335bdd654b33f3605b0c
lackmannicholas <46652944+lackmannicholas@users.noreply.github.com>
2019-05-30 11:33:34 -0700

    Add files via upload
    A static MergeSort class that takes an IList<int>

---
+++ MergeSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public static class MergeSort
+    {
+        public static void Sort(IList<int> list)
+        {
+            int[] helper = new int[list.Count];
+            mergeSort(list, helper, 0, list.Count - 1);
+        }
+
+        private static void mergeSort(IList<int> list, IList<int> helper, int low, int high)
+        {
+            if(low < high)
+            {
+                int middle = low + ((high - low) / 2);
+                mergeSort(list, helper, low, middle);
+                mergeSort(list, helper, middle + 1, high);
+                merge(list, helper, low, middle, high);
+            }
+        }
+
+        private static void merge(IList<int> list, IList<int> helper, int low, int middle, int high)
+        {
+            for(int i = low; i <= high; i++)
+            {
+                helper[i] = list[i];
+            }
+
+            int left = low;
+            int right = middle + 1;
+            int current = low;
+
+            while(left <= middle && right <= high)
+            {
+                if(helper[left] <= helper[right])
+                {
+                    list[current] = helper[left];
+                    left++;
+                }
+                else
+                {
+                    list[current] = helper[right];
+                    right++;
+                }
+                current++;
+            }
+
+            int remaining = middle - left;
+            for(int i = 0; i <= remaining; i++)
+            {
+                list[current + i] = helper[left + i];
+            }
+        }
+    }
+}
