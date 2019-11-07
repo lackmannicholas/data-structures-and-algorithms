3fbb6039f3d9e2e2cf834719e343cc7e764905a9
lackmannicholas <lackmannicholas@gmail.com>
2019-11-06 20:38:46 -0800

    Adding BinaryMinHeap

---
+++ BinaryMinHeap.js
@@ -0,0 +1,37 @@
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+
+class BinaryMinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    Min () {
+        if(this.heap.length == 0)
+            throw Error("Heap is empty");
+            
+        return this.heap[0];
+    }
+
+    RemoveMin() {
+        return this.heap.shift();
+    }
+
+    Insert(item) {
+        this.heap.push(item);
+        let i = this.heap.length - 1;
+        while(i > 0) {
+            let half = Math.floor(i/2);
+            if(this.heap[half] <= this.heap[i])
+                break;
+            this.heap[i] = this.heap[half];
+            this.heap[half] = item;
+
+            i = Math.floor(i/2);
+        }
+    }
+}
