1911aae0c7d61b85c00b354351a8da1640b7d8e5
lackmannicholas <lackmannicholas@gmail.com>
2019-11-07 20:48:52 -0800

    added BST insert and search

--- BST.js
+++ BST.js
@@ -10,7 +10,7 @@ class BST {
         this.root = root;
     }
 
-    printTreePaths = () => {
+    printTreePaths() {
         let result = [];
 
         if(this.root)
@@ -19,7 +19,7 @@ class BST {
         return result;
     }
 
-    buildPath = (node, path, result) => {
+    buildPath(node, path, result) {
         path += node.val.toString() + "->";
 
         // we're at a left because we don't have a left or right child
@@ -38,7 +38,7 @@ class BST {
         return result;
     }
 
-    InOrderTraversal = () => {
+    InOrderTraversal() {
         let result = [];
 
         if(this.root)
@@ -47,7 +47,7 @@ class BST {
         return result;
     }
 
-    inOrderTraversal = (node, result) => {
+    inOrderTraversal (node, result)  {
         if(!node)
             return;
 
@@ -59,8 +59,58 @@ class BST {
 
         return result;
     }
+
+    Insert(item) {
+        let newNode = new TreeNode(item);
+
+        this.root = this.sink(this.root, newNode);
+    }
+
+    sink(node, newNode) {
+        if(!node) 
+            return newNode;
+
+        // if the node we're on is larger than the new node go left
+        if(node.val >= newNode.val){
+            node.left = this.sink(node.left, newNode);
+        }
+        else { // otherwise go right
+            node.right = this.sink(node.right, newNode);
+        }
+
+        return node;
+    }
+
+    Find(item) {
+        return this.search(this.root, item);
+    }
+
+    search(node, item) {
+        if(!node)
+            return null;
+
+        if(node.val == item)
+            return node;
+        
+        if(node.val >= item)
+            return this.search(node.left, item);
+        else
+            return this.search(node.right, item);
+    }
 }
 
+let bst = new BST();
 
+bst.Insert(100);
+bst.Insert(150);
+bst.Insert(4500);
+bst.Insert(12);
+bst.Insert(48);
+bst.Insert(234);
+bst.Insert(10004);
 
-modules.export = BST;
+console.log(bst.InOrderTraversal());
+
+console.log(bst.Find(48));
+console.log(bst.Find(100));
+console.log(bst.Find(200));
