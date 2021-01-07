a47062736cd617c83fd905008781bd6c912373f2
lackmannicholas <lackmannicholas@gmail.com>
2021-01-06 18:06:04 -0800

    adding algorithm selection

--- node/index.js
+++ node/index.js
@@ -1,6 +1,18 @@
 const prompt = require('prompt-sync')();
-console.log('begun');
 
-const app = prompt('Which algorithm do you want to run?');
+const algorithms = [{name: 'Merge Sort', function: () => console.log('Merge Sort function')}]
 
-console.log('end');
+const algo = prompt(`
+Select an algorithm to run:
+  1. Merge Sort
+`);
+
+try {
+  let i = parseInt(algo);
+  console.log(`You chose ${algorithms[i-1].name}`);
+
+  algorithms[i-1].function();
+}
+catch(e) {
+  console.log('You did not enter an acceptable int. Cannot compute. *computer over-heating*  *loud explosion*')
+}
