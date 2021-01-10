27a2614305e0a26866195e037f58487179a51719
lackmannicholas <lackmannicholas@gmail.com>
2021-01-10 14:11:56 -0800

    adding read file from SCC.txt

--- node/StronglyConnectedComponents/index.js
+++ node/StronglyConnectedComponents/index.js
@@ -1,12 +1,26 @@
 const Graph = require("../Graph/GraphAdjArray");
+const fs = require("fs");
 let finishingTime = 0;
 /**
  * Kosaraju's 2-pass Algorithm for computing strongly connected components in a directed graph
  * @param {GraphAdjArray} graph GraphAdjArray Class implementation of a graph
  */
-function StronglyConnectedComponents(graph = new Graph([[6],[4],[8],[0],[7],[7,2],[3,8],[1],[5]])) {
-  console.log(graph);
-  ssc(graph);
+function StronglyConnectedComponents(){//graph = new Graph([[6],[4],[8],[0],[7],[7,2],[3,8],[1],[5]])) {
+  let graph = [];
+  // read input
+  fs.readFile('StronglyConnectedComponents/SCC.txt', 'utf-8', (e, d) => {
+    let edges = d.split('\n');
+    console.log(edges.length +' = 5105043');
+    for(const edge of edges) {
+      let [v, w] = edge.split(' ');
+      if(!graph[v]) graph[v] = [];
+      graph[v].push(w);
+    }
+
+    // pass parsed input to ssc
+    ssc(new Graph(graph));
+  });
+  //ssc(graph);
 }
 
 function ssc(graph) {
@@ -17,16 +31,17 @@ function ssc(graph) {
   // 2. run a DFS-loop on the reversed graph to process node 'finishing times'
   for(let node of graphNot.nodes) {
     if(node.discovered) continue;
-    dfs(graphNot, node, finishingTimes);
+    dfsStack(graphNot, node, finishingTimes);
   }
   // 3. run a DFS-loop on the original graph and process the nodes in decending 'finishing time' order
   for(let n = finishingTimes.length - 1; n >= 0; n--) {
     // have to get the original graph node
     let current = graph.nodes[finishingTimes[n].val];
+    if(!current) continue;
     if(current.discovered) continue;
     leaders.set(current.val, [current]);
     current.discovered = true
-    dfs(graph, current, undefined, leaders, current.val);
+    dfsStack(graph, current, undefined, leaders, current.val);
   }
 
   // using a Map allows you to do this
@@ -40,11 +55,12 @@ function ssc(graph) {
  * @param {GraphAdjArray} graph GraphAdjArray Class implementation of a graph
  * @param {GraphNodeAdjArray} current Current graph node being processed
  */
-function dfs(graph, current, finishingTimes, leaders, leader) {
+function dfsRecursive(graph, current, finishingTimes, leaders, leader) {
   for(const nextNode of current.adj) {
+    if(!nextNode) continue;
     if(!nextNode.discovered) {
       nextNode.discovered = true;
-      console.log(`Discovered ${nextNode.val}`);
+      //console.log(`Discovered ${nextNode.val}`);
 
       dfs(graph, nextNode, finishingTimes, leaders, leader);
 
@@ -57,12 +73,36 @@ function dfs(graph, current, finishingTimes, leaders, leader) {
   }
 }
 
+function dfsStack(graph, s, finishingTimes, leaders, leader) {
+  let stack = [];
+  stack.push(s);
+  while(stack.length > 0) {
+    let current = stack.pop();
+    for(const nextNode of current.adj) {
+      if(!nextNode) continue;
+      if(!nextNode.discovered) {
+        nextNode.discovered = true;
+        //console.log(`Discovered ${nextNode.val}`);
+  
+        stack.push(nextNode);
+
+        if(leaders !== undefined && leader !== undefined) 
+          leaders.get(leader).push(nextNode);
+
+      }
+    }
+    if(finishingTimes)
+      finishingTimes[finishingTime++] = current;
+  }
+}
+
 function reverseGraph(graph) {
   // decompose original graph into reversed adj array array - [[1],[2],[3],[]]
   let graphNot = [];
 
   for(const node of graph.nodes) {
     for(const neighbor of node.adj) {
+      if(!neighbor) continue;
       if(!graphNot[neighbor.val]) graphNot[neighbor.val] = [];
       graphNot[neighbor.val].push(node.val);
     }
